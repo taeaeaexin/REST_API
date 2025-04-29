bd076f020046ed954bc1708a825171540d69104a
taeaeaexin <kjk06119@gmail.com>
2025-04-29 10:30:23 +0900

    json responseentity

---
+++ SpringBootBasicRest/src/main/java/com/mycom/myapp/controller/StudentControllerCrudJsonRequest.java
@@ -0,0 +1,55 @@
+package com.mycom.myapp.controller;
+
+import com.mycom.myapp.dto.StudentDto;
+import com.mycom.myapp.dto.StudentResultDto;
+import com.mycom.myapp.service.StudentServiceCrud;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+//Rest를 적용하면 .api
+// get list : /students
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/json")
+public class StudentControllerCrudJsonRequest {
+    private final StudentServiceCrud studentServiceCrud;
+
+    @GetMapping("/students")
+    public StudentResultDto listStudent() {
+        return studentServiceCrud.listStudent();
+    }
+
+    @GetMapping("/students/{id}")
+    public StudentResultDto detailStudent(@PathVariable("id") Integer id) {
+        return studentServiceCrud.detailStudent(id);
+    }
+
+    // 등록, 수정에 사용되는 StudentDto를 Client에서 JSON으로 보낸다
+    @PostMapping("/students")
+    public StudentResultDto insertStudent(@RequestBody StudentDto studentDto) {
+        return studentServiceCrud.insertStudent(studentDto);
+    }
+
+    @PutMapping("/students/{id}")
+    public StudentResultDto updateStudent(@PathVariable("id") Integer id, StudentDto studentDto) {
+        studentDto.setId(id);
+        return studentServiceCrud.updateStudent(studentDto);
+    }
+
+    @DeleteMapping("/students/{id}")
+    public StudentResultDto deleteStudent(@PathVariable("id") Integer id) {
+        return studentServiceCrud.deleteStudent(id);
+    }
+
+    @GetMapping("/students/count")
+    public StudentResultDto countStudent() {
+        return studentServiceCrud.countStudent();
+    }
+
+    @GetMapping("/students/page")
+    public StudentResultDto listStudent(
+            @RequestParam Integer pageNumber,
+            @RequestParam Integer pageSize) {
+        return studentServiceCrud.listStudent(pageNumber, pageSize);
+    }
+}
