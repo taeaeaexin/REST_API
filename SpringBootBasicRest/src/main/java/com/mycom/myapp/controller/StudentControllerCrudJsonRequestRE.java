60118d090b3a7f97825aa628cd549de94bb90660
taeaeaexin <kjk06119@gmail.com>
2025-04-29 16:24:21 +0900

    250429 강의

--- SpringBootBasicRest/src/main/java/com/mycom/myapp/controller/StudentControllerCrudJsonRequestRE.java
+++ SpringBootBasicRest/src/main/java/com/mycom/myapp/controller/StudentControllerCrudJsonRequestRE.java
@@ -1,5 +1,4 @@
 package com.mycom.myapp.controller;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
